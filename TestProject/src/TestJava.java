520d681011a1c2daadd7971312fae81e52619988
JYOTSNA <JYOTSNA@SriRam.home>
2018-10-11 12:00:28 -0400

    CRM Git Commit

--- TestProject/src/TestJava.java
+++ TestProject/src/TestJava.java
@@ -1,8 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		WebDriver driver=new FirefoxDriver();
+		driver.get ("http://qacrm.herokuapp.com/login");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath(".//*[@id='authentication_username']")).sendKeys("heather");
+		driver.findElement(By.xpath(".//*[@id='authentication_password']")).sendKeys("heather");
+		driver.findElement(By.xpath(".//*[@id='authentication_remember_me']")).click();
+		driver.findElement(By.xpath(".//*[@id='new_authentication']/div[4]/input")).click();
+		driver.findElement(By.xpath(".//*[@id='welcome']/a[4]")).click();
+		
+		
+				
 
 	}
 
